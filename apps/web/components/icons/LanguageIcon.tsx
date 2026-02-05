cf2ead9a0517c8ffda6368e407c7b55a5dfb0814
AKhanjyan <armen.khanjyan200327@mail.ru>
2026-02-05 19:41:31 +0400

    Language

--- apps/web/components/icons/LanguageIcon.tsx
+++ apps/web/components/icons/LanguageIcon.tsx
@@ -6,9 +6,8 @@ interface LanguageIconProps {
 }
 
 export function LanguageIcon({ size = 20, className = '' }: LanguageIconProps) {
-  // Language icon exported from Figma (node-id: 96:926)
-  // If you later download the SVG into /public, you can replace this URL with a local path.
-  const imgLanguageIcon = "http://localhost:3845/assets/1eee720937e2553599a0392b5ba9ee89423b3b80.svg";
+  // Local language icon SVG stored in public/assets/home
+  const imgLanguageIcon = "/assets/home/Vector.svg";
   
   return (
     <img 
